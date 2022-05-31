55e23eeb95f87e7c7e733795dd4ea177cb8ee976
Caio Vinícius <caio1525pereira@gmail.com>
2022-05-31 14:44:07 -0300

    Conclusão dos testes unitários de 'authenticateDeliveryman'

---
+++ src/modules/accounts/useCases/authenticateDeliveryman/AuthenticateDeliverymanUseCase.spec.ts
@@ -0,0 +1,52 @@
+import { hash } from "bcrypt";
+
+import { AppError } from "@shared/errors/AppError";
+import { AccountsRepositoryInMemory } from "@modules/accounts/repositories/in-memory/AccountsRepositoryInMemory";
+import { AuthenticateDeliverymanUseCase } from "./AuthenticateDeliverymanUseCase";
+
+let authenticateDeliverymanUseCase: AuthenticateDeliverymanUseCase;
+let accountsRepositoryInMemory: AccountsRepositoryInMemory;
+
+describe("Authenticate Deliveryman", () => {
+  beforeEach(async () => {
+    accountsRepositoryInMemory = new AccountsRepositoryInMemory();
+    authenticateDeliverymanUseCase = new AuthenticateDeliverymanUseCase(
+      accountsRepositoryInMemory
+    );
+
+    accountsRepositoryInMemory.deliveryman = [
+      {
+        id: "e1679495-9cd7-450e-bdfc-a85cfb918e74",
+        username: "Connor Lloyd",
+        password: await hash("95476106", 10)
+      }
+    ];
+  });
+
+  it("Should be able to authenticate a deliveryman", async () => {
+    const result = await authenticateDeliverymanUseCase.execute({
+      username: "Connor Lloyd",
+      password: "95476106"
+    });
+
+    expect(result).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate a nonexistent deliveryman", async () => {
+    await expect(
+      authenticateDeliverymanUseCase.execute({
+        username: "Katharine Roberson",
+        password: "60353442"
+      })
+    ).rejects.toEqual(new AppError("Username or password invalid!"));
+  });
+
+  it("Should not be able to authenticate a deliveryman with incorrect password", async () => {
+    await expect(
+      authenticateDeliverymanUseCase.execute({
+        username: "Connor Lloyd",
+        password: "84510923"
+      })
+    ).rejects.toEqual(new AppError("Username or password invalid!"));
+  });
+});
