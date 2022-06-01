57603b46c607609ed8931017727c571f76774ce9
Caio Vinícius <caio1525pereira@gmail.com>
2022-06-01 18:03:07 -0300

    Conclusão dos testes de integração de 'find all available controller'

--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableController.spec.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableController.spec.ts
@@ -3,13 +3,14 @@ import request from "supertest";
 import { prisma } from "@database/prismaClient";
 import { app } from "@shared/infra/http/app";
 
-describe("Find All Deliveries Controller - client", () => {
+describe("Find All Available", () => {
   beforeAll(async () => {
     await prisma.$connect();
   });
 
   afterAll(async () => {
     await prisma.deliveries.deleteMany();
+    await prisma.deliveryman.deleteMany();
     await prisma.clients.deleteMany();
     await prisma.$disconnect();
   });
@@ -18,22 +19,25 @@ describe("Find All Deliveries Controller - client", () => {
     await prisma.deliveries.deleteMany();
   });
 
-  it("Should be able to return empty array for a client if there are no deliveries", async () => {
-    const { body: client } = await request(app).post("/client/").send({
-      username: "no deliveries",
-      password: "test123",
-      phone: "+5535940028922"
-    });
+  it("Should be able to return an empty array if no deliveries are available", async () => {
+    const { body: deliveryman } = await request(app)
+      .post("/deliveryman/")
+      .send({
+        username: "no available",
+        password: "test123"
+      });
 
-    const responseToken = await request(app).post("/client/authenticate").send({
-      username: client.username,
-      password: "test123"
-    });
+    const responseToken = await request(app)
+      .post("/deliveryman/authenticate")
+      .send({
+        username: deliveryman.username,
+        password: "test123"
+      });
 
     const { token } = responseToken.body;
 
     const response = await request(app)
-      .get("/client/deliveries")
+      .get("/delivery/available")
       .set({
         Authorization: `Bearer ${token}`
       });
@@ -42,7 +46,7 @@ describe("Find All Deliveries Controller - client", () => {
     expect(response.body).toEqual([]);
   });
 
-  it("Should be able to return all client deliveries", async () => {
+  it("Should be able to return all available deliveries", async () => {
     const { body: client } = await request(app).post("/client/").send({
       username: "client deliveries",
       password: "test123",
@@ -67,15 +71,30 @@ describe("Find All Deliveries Controller - client", () => {
         Authorization: `Bearer ${token}`
       });
 
+    const { body: deliveryman } = await request(app)
+      .post("/deliveryman/")
+      .send({
+        username: "deliveryman",
+        password: "test123"
+      });
+
+    const responseTokenDeliveryman = await request(app)
+      .post("/deliveryman/authenticate")
+      .send({
+        username: deliveryman.username,
+        password: "test123"
+      });
+
+    const { token: tokenDeliveryman } = responseTokenDeliveryman.body;
+
     const response = await request(app)
-      .get("/client/deliveries")
+      .get("/delivery/available")
       .set({
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${tokenDeliveryman}`
       });
 
     expect(response.status).toBe(200);
     expect(Array.isArray(response.body));
-    expect(response.body[0]).toHaveProperty("id");
-    expect(response.body[0]).toHaveProperty("created_at");
+    expect(response.body[0].id_deliveryman).toEqual(null);
   });
 });
